87b38c3a5bb02893f4adf69e87269adc68ae941d
Shashank <mv.shashank123@gmail.com>
2021-05-15 20:46:37 +0530

    dp

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+    height: 70px;
+    background: #090b13;
+    `
+
+const Logo = styled.img`
+    width: 80px;
+    `
+
+const NavMenu = styled.div`
+    
+    `
+
+function Header() {
+    return (
+        <Nav>
+            <Logo src=""/>
+            <NavMenu>
+
+            </NavMenu>
+        </Nav>
+    )
+}
+
+export default Header
